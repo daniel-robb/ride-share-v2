788df78ce68ec42e4f1c9cf44c3f6a2f149a4931
daniel_robb@taylor.edu <drobb@cse10302.cs.local>
2020-11-27 15:24:08 -0500

    Modified all of the files, apparently

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -25,10 +25,10 @@
         <h2 >User Information</h2>
         <br />
         <span>
-          <v-btn height=280 width=220><!--v-on:click="handleSubmit"-->
+          <v-btn v-on:click="helloWorld" height=280 width=220><!--v-on:click="handleSubmit"-->
             <h3>View My Rides</h3>
           </v-btn>
-          <v-btn height=280 width=220><!--v-on:click="handleSubmit"-->
+          <v-btn v-on:click="helloWorld" height=280 width=220><!--v-on:click="handleSubmit"-->
             <h3>Register Driver</h3>
           </v-btn>
         </span>
@@ -36,3 +36,46 @@
     </div>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+
+      snackbar: {
+        show: false,
+        msge: "",
+      },
+    };
+  },
+
+  methods: {
+    logIn() {
+      this.$axios
+        .post("/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((result) => {
+          this.showSnackbar(result.data.msge);
+          if (result.data.ok) {
+            this.$store.commit("logIn", result.data.details);
+            this.$router.push({ name: "home-page" });
+          }
+        })
+        .catch((err) => this.showSnackbar(err));
+    },
+
+    showSnackbar(msge) {
+      this.snackbar.msge = msge;
+      this.snackbar.show = true;
+    },
+
+    helloWorld() {
+      console.log("Words!");
+    },
+  },
+};
+</script>
